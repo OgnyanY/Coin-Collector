c9477b326820eaed4f202045de075f4312c191fe
Danail Nenkov <dpnenkov29@abv.bg>
2023-06-25 14:49:27 +0300

    Coin Exchange Management

---
+++ coin-collector/exchange-management/add_exchange.php
@@ -0,0 +1,37 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "test";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $user_id = $_POST["user_id"];
+  $coin_id = $_POST["coin_id"];
+  $type = $_POST["type"];
+
+  $sql = "INSERT INTO Exchanges (user_id, coin_id, type) VALUES (?, ?, ?)";
+
+  // Prepare and bind
+  $stmt = $conn->prepare($sql);
+  $stmt->bind_param("iis", $user_id, $coin_id, $type);
+
+  // Execute the statement
+  if ($stmt->execute()) {
+    echo "New exchange added successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+
+  $stmt->close();
+}
+
+$conn->close();
+?>

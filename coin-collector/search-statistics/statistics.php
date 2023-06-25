dc5088781bb9a0499eca709df7efc417b4048168
Danail Nenkov <dpnenkov29@abv.bg>
2023-06-25 14:59:23 +0300

    Statistics

---
+++ coin-collector/search-statistics/statistics.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Statistics</title>
+</head>
+<body>
+    <h2>Statistics</h2>
+
+    <?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "test";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Check connection
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }
+
+    // Number of coins
+    $sql = "SELECT COUNT(*) AS num_coins FROM Coins";
+    $result = $conn->query($sql);
+    $row = $result->fetch_assoc();
+    echo "<p>Number of coins: " . $row["num_coins"] . "</p>";
+
+    // Number of collections
+    $sql = "SELECT COUNT(*) AS num_collections FROM Collections";
+    $result = $conn->query($sql);
+    $row = $result->fetch_assoc();
+    echo "<p>Number of collections: " . $row["num_collections"] . "</p>";
+
+    // Number of exchanges
+    $sql = "SELECT COUNT(*) AS num_exchanges FROM Exchanges";
+    $result = $conn->query($sql);
+    $row = $result->fetch_assoc();
+    echo "<p>Number of exchanges: " . $row["num_exchanges"] . "</p>";
+
+    $conn->close();
+    ?>
+</body>
+</html>

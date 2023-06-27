2a1c1fd6e47c46430f9e3e88f517bb2158ed90b4
OgnyanY <o.yonchev@abv.bg>
2023-06-27 16:05:14 +0300

    Final touches

--- coin-collector/collection-management/view_collections.php
+++ coin-collector/collection-management/view_collections.php
@@ -42,7 +42,7 @@
     if ($result->num_rows > 0) {
       // Output data of each row
       while ($row = $result->fetch_assoc()) {
-        echo "<h3>" . $row["name"] . "</h3>";
+        echo "<h3 style='border-top: 3px solid black;'>" . $row["name"] . "</h3>";
 
         // Fetch coins in the collection
         $sql2 = "SELECT Coins.name, Coins.country, Coins.year, Coins.value, Coins.image_front, Coins.image_back FROM Collection_Coins JOIN Coins ON Collection_Coins.coin_id = Coins.id WHERE Collection_Coins.collection_id = " . $row["id"];
@@ -53,8 +53,8 @@
           while ($row2 = $result2->fetch_assoc()) {
             echo "<div class='coin_row'>";
             echo "<p>" . $row2["name"] . " (" . $row2["country"] . ", " . $row2["year"] . ") - " . $row2["value"] . "</p>";
-            echo "<div class='coin_images'><img src='" . $row["image_front"] . "' alt='Image Front' class='coin_image'>";
-            echo "<img src='" . $row["image_back"] . "' alt='Image Back' class='coin_image'></div>";
+            echo "<div class='coin_images'><img src='" . $row2["image_front"] . "' alt='Image Front' class='coin_image'>";
+            echo "<img src='" . $row2["image_back"] . "' alt='Image Back' class='coin_image'></div>";
             echo "</div>";
           }
         } else {

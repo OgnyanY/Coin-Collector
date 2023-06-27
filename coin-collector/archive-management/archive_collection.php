e8317c6f14b798bbd1c340d7eb9010bc9198c1d5
OgnyanY <o.yonchev@abv.bg>
2023-06-27 12:00:39 +0300

    Fixing

--- coin-collector/archive-management/archive_collection.php
+++ coin-collector/archive-management/archive_collection.php
@@ -24,6 +24,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   // Execute the statement
   if ($stmt->execute()) {
     echo "Collection archived successfully";
+    header("Location: ../successful.html");
     
   } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
--- coin-collector/archive-management/archived_collections.php
+++ coin-collector/archive-management/archived_collections.php
@@ -18,37 +18,42 @@
   </div>
 
   <h2>Archived Collections</h2>
+  <div class="php_generated">
 
-  <?php
-  $servername = "localhost";
-  $username = "root";
-  $password = "";
-  $dbname = "test"; // replace with your database name
-  
-  // Create connection
-  $conn = new mysqli($servername, $username, $password, $dbname);
+    <?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "test"; // replace with your database name
+    
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
-  // Check connection
-  if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-  }
-
-  $sql = "SELECT Users.username, Collections.name FROM Collections JOIN Users ON Collections.user_id = Users.id WHERE Collections.is_archived = TRUE";
-
-  $result = $conn->query($sql);
-
-  if ($result->num_rows > 0) {
-    // output data of each row
-    while ($row = $result->fetch_assoc()) {
-      echo "<h3>" . $row["name"] . "</h3>";
-      echo "<p>Owned by: " . $row["username"] . "</p>";
+    // Check connection
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
     }
-  } else {
-    echo "No archived collections found";
-  }
 
-  $conn->close();
-  ?>
+    $sql = "SELECT Users.username, Collections.name FROM Collections JOIN Users ON Collections.user_id = Users.id WHERE Collections.is_archived = TRUE";
+
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+      // output data of each row
+      while ($row = $result->fetch_assoc()) {
+        echo "<div class='coin_row'>";
+        echo "<h3>" . $row["name"] . "</h3>";
+        echo "<p>Owned by: " . $row["username"] . "</p>";
+        echo "</div>";
+      }
+    } else {
+      echo "No archived collections found";
+    }
+
+    $conn->close();
+    ?>
+
+  </div>
 </body>
 
 </html>

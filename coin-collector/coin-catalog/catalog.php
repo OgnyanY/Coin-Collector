4809b4148731ac4452569c7c03873bed0ced63a1
OgnyanY <o.yonchev@abv.bg>
2023-06-27 11:48:31 +0300

    Update the css for the main page

--- coin-collector/coin-catalog/catalog.php
+++ coin-collector/coin-catalog/catalog.php
@@ -17,42 +17,45 @@
     </div>
   </div>
 
-  <?php
-  $servername = "localhost";
-  $username = "root";
-  $password = "";
-  $dbname = "test";
+  <div class="php_generated">
+    <?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "test";
 
-  // Create connection
-  $conn = new mysqli($servername, $username, $password, $dbname);
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
-  // Check connection
-  if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-  }
-
-  $sql = "SELECT name, country, year, value, image_front, image_back FROM Coins";
-
-  $result = $conn->query($sql);
-
-  if ($result->num_rows > 0) {
-    // output data of each row
-    while ($row = $result->fetch_assoc()) {
-      echo "<div class='coin_row'>";
-      echo "<h3>" . $row["name"] . "</h3>";
-      echo "<p>Country: " . $row["country"] . "</p>";
-      echo "<p>Year: " . $row["year"] . "</p>";
-      echo "<p>Value: " . $row["value"] . "</p>";
-      echo "<div class='coin_images'><img src='" . $row["image_front"] . "' alt='Image Front' class='coin_image'>";
-      echo "<img src='" . $row["image_back"] . "' alt='Image Back' class='coin_image'></div>";
-      echo "</div>";
+    // Check connection
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
     }
-  } else {
-    echo "No coins found";
-  }
 
-  $conn->close();
-  ?>
+    $sql = "SELECT name, country, year, value, image_front, image_back FROM Coins";
+
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+      // output data of each row
+      while ($row = $result->fetch_assoc()) {
+        echo "<div class='coin_row'>";
+        echo "<h3>" . $row["name"] . "</h3>";
+        echo "<p>Country: " . $row["country"] . "</p>";
+        echo "<p>Year: " . $row["year"] . "</p>";
+        echo "<p>Value: " . $row["value"] . "</p>";
+        echo "<div class='coin_images'><img src='" . $row["image_front"] . "' alt='Image Front' class='coin_image'>";
+        echo "<img src='" . $row["image_back"] . "' alt='Image Back' class='coin_image'></div>";
+        echo "</div>";
+      }
+    } else {
+      echo "No coins found";
+    }
+
+    $conn->close();
+    ?>
+
+  </div>
 </body>
 
 </html>
